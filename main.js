42910ce310bfa7bb7d4c1b1b7afd1a9a7d3b194c
Jovaughn E <jovaughn.ewing@ufl.edu>
2021-03-22 12:05:05 -0600

    Added JS function

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 // It is always helpful to use comments in your code!
+const freezingTemp = 32;
+
+const boilingTemp = 212;
+
+function coldEnough(deg) {
+    const freezingTemp = 32;
+    if (freezingTemp <= deg) {
+        return deg + "is below freezing. ";
+    } else {
+        return deg + " is above freezing. ";
+    }
+}
+
+document.getElementById()
