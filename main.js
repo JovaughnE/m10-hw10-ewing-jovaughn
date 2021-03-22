cbb34e54957abdfe10d904e8487a15eac23617a5
Jovaughn E <jovaughn.ewing@ufl.edu>
2021-03-22 16:35:35 -0600

    Corrected JS.

--- main.js
+++ main.js
@@ -6,40 +6,27 @@ const freezingTemp = 32;
 
 const boilingTemp = 212;
 
-let waterTemp = deg => document.getElementById('water-temp').innerHTML = 299;
+let waterTemp = 299;
 
+document.getElementById('water-temp').innerHTML = waterTemp;
+
+console.log(waterTemp);
 //Definition of a function to decide if the temp is below or above freezing
 
-function coldEnough(deg) {
-    if (deg <= freezingTemp) {
-        return deg + "is below freezing. ";
+function compareTemp(deg) {
+    if (deg <= 32) {
+        document.getElementById('frozen').classList.add('show');
+    } else if (deg >= 212) {
+        document.getElementById('boiling').classList.add('show');
     } else {
-        return deg + " is above freezing. ";
+        document.getElementById('good-temp').classList.add('show');
     }
+    return deg;
 }
-
-//Definition of a function to decide if the temp is below or above boiling
-
-function tooHot(deg) {
-    if (deg >= boilingTemp) {
-        return deg + "is above boiling. ";
-    } else {
-        return deg + " is below boiling. ";
-    }
-}
-
-setTimeout( function() {
-   document.getElementById('boiling').innerHTML = "The water is HOT, I think it is boiling";
-}, 3000);
 
 //Output constants in the corresponding span in the HTML Document.
 document.getElementById('boiling-temp').innerHTML = 212;
 document.getElementById('freezing-temp').innerHTML = 32;
-document.getElementById('water-temp').innerHTML = waterTemp();
 
-//Invocation of functions
-
-coldEnough();
-
-tooHot() 
-    document.getElementById('boiling').classList.add('show');
+//Let's find our function compareTemp, wait 3 seconds, then pass in waterTemp
+setTimeout(compareTemp, 3000, waterTemp);
